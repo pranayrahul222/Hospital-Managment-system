e7b267ff7e19e831d3af8d81bdd55f488fc4c024
pranaya.rahul222@gmail.com <pranaya.rahul222@gmail.com>
2020-10-22 16:26:27 +0530

    complete basic structure

--- HospitalMgmt/HospitalMgmt/urls.py
+++ HospitalMgmt/HospitalMgmt/urls.py
@@ -24,4 +24,14 @@ urlpatterns = [
     path('', Index,name='home'),
     path('admin_login/', Login,name='login'),
     path('logout/', Logout_admin,name='logout'),
+    path('view_doctor/',View_Doctor,name='view_doctor'),
+    path('add_doctor/',Add_Doctor,name='add_doctor'),
+    path('delete_doctor(?P<int:pid>)/',Delete_Doctor,name='delete_doctor'),
+    path('view_patient/',View_Patient,name='view_patient'),
+    path('add_patient/',Add_Patient,name='add_patient'),
+    path('delete_patient(?P<int:pid>)/',Delete_Patient,name='delete_patient'),
+    path('view_appointment/',View_Appointment,name='view_appointment'),
+    path('add_appointment/',Add_Appointment,name='add_appointment'),
+    path('delete_appointment(?P<int:pid>)/',Delete_Appointment,name='delete_appointment'),
+    
 ]
